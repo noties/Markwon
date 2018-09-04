472e3de7185077e21f06fc4559962b178430bc63
Dimitry Ivanov <mail@dimitryivanov.ru>
2018-09-04 16:36:08 +0300

    Add favicon to documentation site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,11 @@ module.exports = {
     title: 'Markwon',
     description: 'Android markdown library based on commonmark specification',
     head: [
-        ['link', {rel: 'icon', href: '/favicon.png'}]
+        ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=1'}],
+        ['link', {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=1'}],
+        ['link', {rel: 'icon', href: '/favicon.ico?v=1'}],
+        ['link', {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=1'}],
+        ['link', {rel: 'manifest', href: '/manifest.json?v=1'}],
     ],
     themeConfig: {
         nav: [
