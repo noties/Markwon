0505845e4ce259f0b585aefeee0aa25167bcf3da
Dimitry Ivanov <mail@dimitryivanov.ru>
2018-12-28 14:19:36 +0300

    Add sample module and update app icons

---
+++ sample/src/main/java/ru/noties/markwon/sample/MainActivity.java
@@ -0,0 +1,12 @@
+package ru.noties.markwon.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
