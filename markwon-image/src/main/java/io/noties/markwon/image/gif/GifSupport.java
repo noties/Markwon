1b7fbfb77f7b3cd29c88cc8e970afba4cbe928af
Dimitry Ivanov <di@noties.io>
2019-08-26 13:53:39 +0300

    ImagesPlugin print a warning for SVG and GIF instead of full stacktrace

--- markwon-image/src/main/java/io/noties/markwon/image/gif/GifSupport.java
+++ markwon-image/src/main/java/io/noties/markwon/image/gif/GifSupport.java
@@ -1,5 +1,9 @@
 package io.noties.markwon.image.gif;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 /**
  * @since 4.0.0
  */
@@ -13,8 +17,9 @@ public abstract class GifSupport {
             pl.droidsonroids.gif.GifDrawable.class.getName();
             result = true;
         } catch (Throwable t) {
-            //  @since 4.1.1-SNAPSHOT do not print stacktrace (it can become noisy)
-//            t.printStackTrace();
+            // @since 4.1.1-SNAPSHOT instead of printing full stacktrace of the exception,
+            // just print a warning to the console
+            Log.w("MarkwonImagesPlugin", missingMessage());
             result = false;
         }
         HAS_GIF = result;
@@ -22,6 +27,16 @@ public abstract class GifSupport {
 
     public static boolean hasGifSupport() {
         return HAS_GIF;
+    }
+
+    /**
+     * @since 4.1.1-SNAPSHOT
+     */
+    @NonNull
+    static String missingMessage() {
+        return "`pl.droidsonroids.gif:android-gif-drawable:*` " +
+                "dependency is missing, please add to your project explicitly if you " +
+                "wish to use GIF media-decoder";
     }
 
     private GifSupport() {
--- markwon-image/src/main/java/io/noties/markwon/image/svg/SvgSupport.java
+++ markwon-image/src/main/java/io/noties/markwon/image/svg/SvgSupport.java
@@ -1,5 +1,9 @@
 package io.noties.markwon.image.svg;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 /**
  * @since 4.0.0
  */
@@ -13,8 +17,9 @@ public abstract class SvgSupport {
             com.caverock.androidsvg.SVG.class.getName();
             result = true;
         } catch (Throwable t) {
-            //  @since 4.1.1-SNAPSHOT do not print stacktrace (it can become noisy)
-//            t.printStackTrace();
+            // @since 4.1.1-SNAPSHOT instead of printing full stacktrace of the exception,
+            // just print a warning to the console
+            Log.w("MarkwonImagesPlugin", missingMessage());
             result = false;
         }
         HAS_SVG = result;
@@ -22,6 +27,15 @@ public abstract class SvgSupport {
 
     public static boolean hasSvgSupport() {
         return HAS_SVG;
+    }
+
+    /**
+     * @since 4.1.1-SNAPSHOT
+     */
+    @NonNull
+    static String missingMessage() {
+        return "`com.caverock:androidsvg:*` dependency is missing, " +
+                "please add to your project explicitly if you wish to use SVG media-decoder";
     }
 
     private SvgSupport() {
